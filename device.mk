d6dc9731ec17dabc5dfb276f4080f9da6acac158
Sabar <31942277+sabarop@users.noreply.github.com>
2024-09-17 13:49:44 +0700

    platina: overlay: Convert to RRO

--- device.mk
+++ device.mk
@@ -51,6 +51,14 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    PlatinaApertureOverlay \
+    PlatinaFrameworksOverlay \
+    PlatinaNoCutoutOverlay \
+    PlatinaSettingsOverlay \
+    PlatinaSettingsProviderOverlay \
+    PlatinaSystemUIOverlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml
